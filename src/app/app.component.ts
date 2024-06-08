6ba5b0dfc926ac20f38567a1fc1462ff02304c0d
Gayathridevi <gayathridevi@neverskip.com>
2024-06-08 16:04:32 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,138 @@
+import { ChangeDetectorRef, Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit{
+  title = 'assessment';
+  displayedColumns: string[] = ['title', 'image', 'collabrator', 'privacy'];
+  dataSource = new MatTableDataSource();
+  listData : any = [
+  ]
+  customerData: any = [];
+  customerForm!: FormGroup;
+  regions = [
+    { value: 'north', viewValue: 'North' },
+    { value: 'outh', viewValue: 'South' },
+    { value: 'east', viewValue: 'East' },
+    { value: 'west', viewValue: 'West' }
+  ];
+  countries = [
+    { value: 'usa', viewValue: 'USA' },
+    { value: 'canada', viewValue: 'Canada' },
+    { value: 'exico', viewValue: 'Mexico' }
+  ];
+  form!: FormGroup;
+  collaborators : any = [
+    // { text: 'Collaborator 1', value: '1' },
+    // { text: 'Collaborator 2', value: '2' },
+    // { text: 'Collaborator 3', value: '3' }
+  ];
+  @ViewChild('customer') customer!: TemplateRef<any>;
+  @ViewChild('pin') pin!: TemplateRef<any>;
+  selImg: any;
+  constructor(private dialog: MatDialog,private cd : ChangeDetectorRef,private sanitizer: DomSanitizer) {
+
+  }
+  ngOnInit() {
+    this.dataSource.data = this.listData;
+    console.log(this.dataSource.data)
+    this.customerForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      region: new FormControl('', Validators.required),
+      country: new FormControl()
+    });
+    this.form = new FormGroup({
+      title: new FormControl('', Validators.required),
+      image: new FormControl('', Validators.required),
+      collaborator: new FormControl('', Validators.required),
+      privacy: new FormControl('private')
+    });
+  }
+  get isValid() {
+    return this.customerForm.get('name')?.valid &&
+           this.customerForm.get('email')?.valid &&
+           this.customerForm.get('region')?.valid;
+  }
+  sanitizedImageUrl: any;
+
+  getImageUrl(imagePath: string): void {
+    const file = new File([imagePath], imagePath);
+    const url = URL.createObjectURL(file);
+    this.sanitizedImageUrl = this.sanitizer.bypassSecurityTrustUrl(url);
+  }
+  handleFileInput(event:any) {
+    // const input = event.target;
+
+    // if (input.files && input.files[0]) {
+    //   const file = input.files[0];
+    //   const reader = new FileReader();
+
+    //   reader.onload = (e) => {
+    //     this.selImg = e.target?.result;
+    //     if (typeof this.selImg === 'string') {
+    //       this.form.value.image  = this.selImg;
+    //     }
+    //   };
+
+    //   reader.readAsDataURL(file);
+    // }
+    const input = event.target;
+
+  if (input.files && input.files[0]) {
+    const file = input.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      this.selImg = e.target?.result;
+      this.form.value.image  = this.selImg;
+    };
+
+    reader.readAsDataURL(file);
+  }
+  }
+  onSubmit(formValue: any): void {
+    
+    this.customerData.push(formValue);
+    console.log('customerData', this.customerData);
+    this.collaborators.push(formValue);
+    this.customerForm.reset();
+    this.dialog.closeAll();
+    // Save the form data to your database or API
+  }
+  onSubmitPin() {
+    console.log('Form Value', this.form.value);
+    this.dialog.closeAll();
+    // this.getImageUrl(this.form.value.image);
+    // this.form.value.image = this.sanitizedImageUrl;
+    this.listData.push(this.form.value);
+    this.dataSource.data = [...this.listData]
+    this.cd.detectChanges();
+    console.log(this.dataSource.data)
+    this.form.reset();
+
+  }
+  addcustomer() {
+    this.customerForm.reset();
+    this.dialog.open(this.customer,{disableClose: true});
+  }
+  cancel() {
+    this.customerForm.reset();
+    this.dialog.closeAll();
+  }
+  addpin() {
+    this.form.reset();
+    this.form.value.privacy = 'private';
+    this.form.controls['privacy'].setValue('private');
+    this.dialog.open(this.pin,{disableClose: true});
+
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule} from '@angular/material/table'
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule} from '@angular/material/icon'
+import { MatRadioModule} from '@angular/material/radio'
+import { FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatButtonModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatRadioModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
